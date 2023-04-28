eb91f9ea1a51ccbd6149adb599078d8fa6a80fba
Stefan Rummer <tenderport.info@gmail.com>
2023-04-28 08:43:54 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+# import pandas as pd
+
+
+def run_app():
+
+    # build expander and place the app inside
+    with st.expander('sdfjkdf', expanded=True):
+        st.write('TUM.ai Makeathon')
+
+
+if __name__ == '__main__':
+
+    # run the main app function
+    run_app()
